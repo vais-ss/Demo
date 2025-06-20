99df60fa97f09dcc7307ddf83c6a5ea672b73d74
SWYOD <67761337+SWYOD@users.noreply.github.com>
2025-06-20 16:00:23 +0300

    Adding Swagger

--- back/src/app/main.ts
+++ back/src/app/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  app.enableCors();
+
+  SwaggerModule.setup('api', app, document);
+  await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
